f20e2516eef97cbed8b3f12d798d467b88a4c8a8
Crowdbotics <team@crowdbotics.com>
2020-10-16 22:18:10 +0000

    Update models

--- backend/promote_the_truth_21595/wsgi.py
+++ backend/promote_the_truth_21595/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'promote_the_truth_21595.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "promote_the_truth_21595.settings")
 
 application = get_wsgi_application()
